2c9800a2757bf68c0f6f405eee9ae2918471312b
Amirshah98760 <amirshah98760@gmail.com>
2024-09-06 21:49:59 +0500

    Navbar Created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div>
-            <div>
+        <nav className='flex justify-between px-5 p-2'>
+            <div className='flex justify-center items-center gap-x-4'>
             
-                <img src="https://tse1.mm.bing.net/th?id=OIP.g5K8l_AA_9GEXSD16ypjtgHaHa&pid=Api" alt="" />
-             <h2>Wow cafe</h2>
+                <img src="https://tse1.mm.bing.net/th?id=OIP.g5K8l_AA_9GEXSD16ypjtgHaHa&pid=Api" alt="" className='w-[60px] h-[60px] cursor-pointer'/>
+             <h2 className='font-bold text-gray-600 text-lg cursor-pointer'>WOW CAFE</h2>
             </div>
-        </div>
+            <div className='flex justify-center items-center gap-x-5'>
+                <h2 className='text-gray-600 hover:text-pink-500 cursor-pointer font-semibold'>Home</h2>
+                <h2 className='text-gray-600 hover:text-pink-500 cursor-pointer font-semibold'>About</h2>
+                <h2 className='text-gray-600 hover:text-pink-500 cursor-pointer font-semibold'>Foods</h2>
+                <h2 className='text-gray-600 hover:text-pink-500 cursor-pointer font-semibold'>Contact</h2>
+            </div>
+        </nav>
     )
 }
 
