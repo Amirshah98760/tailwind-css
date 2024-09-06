97414234fbf9ad715c2740bd3ab149bd01267f77
Amirshah98760 <amirshah98760@gmail.com>
2024-09-06 21:00:01 +0500

    tailwind css import

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <div>
+            
+                <img src="https://tse1.mm.bing.net/th?id=OIP.g5K8l_AA_9GEXSD16ypjtgHaHa&pid=Api" alt="" />
+             <h2>Wow cafe</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
